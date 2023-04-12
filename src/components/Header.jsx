60607f13e6e23d5a06ca81d39a578be9f40bbf77
Rho <117315116+idrodrigo@users.noreply.github.com>
2023-04-12 14:19:14 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <h1
         className='text-center text-3xl md:text-4xl text-zinc-800 font-bold mt-5 dark:text-gray-200 relative uppercase mb-10'
       >
-        MONITORING OF PATIENTS{' '}
+        MANAGEMENT PATIENTS{' '}
         <span className='text-indigo-600 after:border-b-indigo-600 dark:after:border-b-sky-400 after:content-center after:absolute after:w-20 md:after:w-60 after:-bottom-4 after:left-0 after:right-0 after:mx-auto dark:text-sky-400'>
           VETERINARY
         </span>
